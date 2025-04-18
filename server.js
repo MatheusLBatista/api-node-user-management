754e9dae7136097a103c8463d7449b7692982ae9
Matheus Batista <matheusifro2020@gmail.com>
2025-04-18 15:35:50 -0400

    creating server and get and post routes

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+const users = [];
+
+app.use(express.json())
+
+/*
+    - Criar nossa API de Usuários
+    - Criar um usuário
+    - Listar todos os usuários
+    - Editar um usuários
+    - Deletar um usuarios
+*/
+
+app.get('/users', (req, res) => {
+    res.status(200).json(users)
+})
+
+app.post('/users', (req, res) => {
+    users.push(req.body)
+
+    res.status(201).json(req.body);
+})
+
+app.listen(5001);
